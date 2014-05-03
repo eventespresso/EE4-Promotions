7a254ab3e916ca86a1d6a0904afaef545ec98be4
Darren Ethier <darren@roughsmootheng.in>
2014-05-03 15:52:22 -0400

    add unit tests framework to EE4 Promotions [touch: #5800]

---
+++ tests/testcases/espresso_promotions_tests.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Contains test class for espresso_promotions.php
+ *
+ * @since  		1.0.0
+ * @package 		EE4 Promotions
+ * @subpackage 	tests
+ */
+
+
+/**
+ * Test class for espresso_promotions.php
+ *
+ * @since 		1.0.0
+ * @package 		EE4 Promotions
+ * @subpackage 	tests
+ */
+class espresso_promotions_tests extends EE_UnitTestCase {
+
+	/**
+	 * Tests the loading of the main promotions file
+	 *
+	 * @since 1.0.0
+	 */
+	function test_loading_promotions() {
+		$this->assertEquals( has_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_promotions'), 10 );
+		$this->assertTrue( class_exists( 'EE_Promotions' ) );
+	}
+}
