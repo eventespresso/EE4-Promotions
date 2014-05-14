48b715245dc329205c51556a540f25c9210bb072
Darren Ethier <darren@roughsmootheng.in>
2014-05-13 21:13:29 -0400

    modify tests to account for new priority on loading espresso promotions [touch: 5852]

--- tests/testcases/espresso_promotions_tests.php
+++ tests/testcases/espresso_promotions_tests.php
@@ -23,7 +23,7 @@ class espresso_promotions_tests extends EE_UnitTestCase {
 	 * @since 1.0.0
 	 */
 	function test_loading_promotions() {
-		$this->assertEquals( has_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_promotions'), 10 );
+		$this->assertEquals( has_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_promotions'), 5 );
 		$this->assertTrue( class_exists( 'EE_Promotions' ) );
 	}
 }
