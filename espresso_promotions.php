6b865e650ddf43e8e987b38be3cbc557c078b70c
Darren Ethier <darren@roughsmootheng.in>
2014-05-18 16:16:21 -0400

    bump version of promotions to 1.0.0.dev.004

--- espresso_promotions.php
+++ espresso_promotions.php
@@ -3,7 +3,7 @@
   Plugin Name: Event Espresso - Promotions
   Plugin URI: http://www.eventespresso.com
   Description: Help promote your Events with Event Espresso Promotions by offering discounts to increase registrations and get more cheap skate people out to your events!
-  Version: 1.0.0.dev.003
+  Version: 1.0.0.dev.004
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
   Copyright 2014 Event Espresso (email : support@eventespresso.com)
@@ -38,7 +38,7 @@
  */
 function load_espresso_promotions() {
 	// promotions version
-	define( 'EE_PROMOTIONS_VERSION', '1.0.0.dev.003' );
+	define( 'EE_PROMOTIONS_VERSION', '1.0.0.dev.004' );
 	require_once ( plugin_dir_path( __FILE__ ) . 'EE_Promotions.class.php' );
 	EE_Promotions::register_addon();
 }
