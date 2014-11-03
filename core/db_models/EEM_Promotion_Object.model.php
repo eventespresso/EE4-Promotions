cdc57a897a51a6026d99d9008b886ac4ea341f85
Brent Christensen <brent@eventespresso.com>
2014-11-03 11:17:34 -0800

    fix typos,  phpdoc, instance check [touch:3456]

--- core/db_models/EEM_Promotion_Object.model.php
+++ core/db_models/EEM_Promotion_Object.model.php
@@ -1,51 +1,47 @@
 <?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+require_once ( EE_MODELS . 'EEM_Base.model.php' );
 /**
- * Event Espresso
- *
- * Event Registration and Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author				Seth Shoultes
- * @ copyright		(c) 2008-2011 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link					http://www.eventespresso.com
- * @ version		 	4.0
- *
- * ------------------------------------------------------------------------
+ * EEM_Promotion_Object
  *
  * Promotion-to-almost-anything Model. Establishes that a certain promotion
  * CAN BE USED on certain objects
  *
  * @package			Event Espresso
  * @subpackage		includes/models/
- * @author				Michael Nelson
+ * @author			Michael Nelson
  *
- * ------------------------------------------------------------------------
  */
-require_once ( EE_MODELS . 'EEM_Base.model.php' );
-
 class EEM_Promotion_Object extends EEM_Base {
 
-  	// private instance of the Attendee object
+	/**
+	 * @var EEM_Promotion_Object
+	 */
 	private static $_instance = NULL;
 
+
+
 	/**
-	 *		This funtion is a singleton method used to instantiate the EEM_Attendee object
+	 *		This function is a singleton method used to instantiate the EEM_Promotion_Object object
 	 *
 	 *		@access public
-	 *		@return EEM_Attendee instance
-	 */	
+	 *		@return EEM_Promotion_Object
+	 */
 	public static function instance(){
-	
+
 		// check if instance of EEM_Attendee already exists
-		if ( self::$_instance === NULL ) {
-			// instantiate Espresso_model 
+		if ( ! self::$_instance instanceof EEM_Promotion_Object ) {
+			// instantiate Espresso_model
 			self::$_instance = new self();
 		}
-		// EEM_Attendee object
 		return self::$_instance;
 	}
 
+
+
+	/**
+	 * 	constructor
+	 * @return EEM_Promotion_Object
+	 */
 	protected function __construct(){
 		$this->singular_item = __('Status','event_espresso');
 		$this->plural_item = __('Stati','event_espresso');
@@ -60,7 +56,7 @@ class EEM_Promotion_Object extends EEM_Base {
 				'OBJ_ID'=>new EE_Foreign_Key_Int_Field('OBJ_ID', __("ID of the Related Object", "event_espresso"), false, 0, $relations),
 				'POB_type'=>new EE_Any_Foreign_Model_Name_Field('POB_type', __("Model of Related Object", "event_espresso"),false, 'Event',$relations),
 				'POB_used'=>new EE_Integer_Field('POB_used', __("Times the promotion has been used for this object", "event_espresso"), false,0)
-				
+
 			));
 		$this->_model_relations = array(
 			'Event'=>new EE_Belongs_To_Any_Relation(),
@@ -70,12 +66,12 @@ class EEM_Promotion_Object extends EEM_Base {
 			'Transaction'=>new EE_Belongs_To_Any_Relation(),
 			'Promotion'=>new EE_Belongs_To_Relation(),
 		);
-		
+
 		parent::__construct();
 	}
 
 
 }
-// 
+//
 // End of file EEM_Promotion_Object.model.php
 // Location: /includes/models/EEM_Promotion_Object.model.php
