b68143be2e557f2e1e4ff4553e4c0d09cd18f7d7
Darren Ethier <darren@roughsmootheng.in>
2015-07-06 21:23:50 -0400

    implement interfaces [touch: 8466]
    implements EEI_Admin_Links and EEI_Line_Item_Object interfaces

--- core/db_classes/EE_Promotion.class.php
+++ core/db_classes/EE_Promotion.class.php
@@ -17,7 +17,7 @@ if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit('NO direct script access allo
  * @subpackage models
  * @author Mike Nelson
  */
-class EE_Promotion extends EE_Soft_Delete_Base_Class{
+class EE_Promotion extends EE_Soft_Delete_Base_Class implements EEI_Line_Item_Object, EEI_Admin_Links {
 
 	//constants
 
@@ -866,4 +866,93 @@ class EE_Promotion extends EE_Soft_Delete_Base_Class{
 	}
 
 
+
+
+	/**
+	 * Implementation for EEI_Line_Item object method.
+	 * @see EEI_Line_Item_Object for comments
+	 * @return string
+	 */
+	public function get_object_name() {
+		return $this->name();
+	}
+
+
+	/**
+	 * Implementation for EEI_Line_Item object method.
+	 * @see EEI_Line_Item_Object for comments
+	 * @return int
+	 */
+	public function get_ID() {
+		return $this->ID();
+	}
+
+
+
+
+	/**
+	 * Implementation for EEI_Admin_Links interface method.
+	 * @see EEI_Admin_Links for comments
+	 * @return string
+	 */
+	public function get_admin_details_link() {
+		return $this->get_admin_edit_link();
+	}
+
+
+
+
+
+
+	/**
+	 * Implementation for EEI_Admin_Links interface method.
+	 * @see EEI_Admin_Links for comments
+	 * @return return string
+	 */
+	public function get_admin_edit_link() {
+		EE_Registry::instance()->load_helper('URL');
+		return EEH_URL::add_query_args_and_nonce( array(
+			'page' => 'espresso_promotions',
+			'action' => 'edit',
+			'PRO_ID' => $this->ID()
+		),
+			admin_url( 'admin.php' )
+		);
+	}
+
+
+
+	/**
+	 * Implementation for EEI_Admin_Links interface method.
+	 * @see EEI_Admin_Links for comments
+	 * @return string
+	 */
+	public function get_admin_settings_link() {
+		EE_Registry::instance()->load_helper('URL');
+		return EEH_URL::add_query_args_and_nonce( array(
+			'page' => 'espresso_promotions',
+			'action' => 'basic_settings'
+		),
+			admin_url( 'admin.php' )
+		);
+	}
+
+
+
+
+
+	/**
+	 * Implementation for EEI_Admin_Links interface method.
+	 * @see EEI_Admin_Links for comments
+	 * @return string
+	 */
+	public function get_admin_overview_link() {
+		EE_Registry::instance()->load_helper('URL');
+		return EEH_URL::add_query_args_and_nonce( array(
+			'page' => 'espresso_promotions',
+			'action' => 'default'
+		),
+			admin_url( 'admin.php' )
+		);
+	}
 }
