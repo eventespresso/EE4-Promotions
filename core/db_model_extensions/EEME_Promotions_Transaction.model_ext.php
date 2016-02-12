b164859c29e644f1436d1a36542f77f7c1229021
Mike Nelson <michael@eventespresso.com>
2016-02-12 10:03:45 -0800

    [touch:9173]{t:10}allow deletion of events and transactions even if they have related promotion_objects because those promotion objects will get deleted automatically

--- core/db_model_extensions/EEME_Promotions_Transaction.model_ext.php
+++ core/db_model_extensions/EEME_Promotions_Transaction.model_ext.php
@@ -16,7 +16,7 @@ class EEME_Promotions_Transaction extends EEME_Base{
 	public function __construct() {
 		$this->_model_name_extended = 'Transaction';
 		$this->_extra_relations = array(
-			'Promotion_Object'=>new EE_Has_Many_Any_Relation()
+			'Promotion_Object'=>new EE_Has_Many_Any_Relation( false )
 		);
 		parent::__construct();
 	}
